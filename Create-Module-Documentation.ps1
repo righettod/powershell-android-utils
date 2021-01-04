8995d40ce55bc66ec55655c50c313c7cf0965bb5
drighetto <drighetto@excellium-services.com>
2021-01-04 16:55:36 +0100

    Debug gen doc

--- Create-Module-Documentation.ps1
+++ Create-Module-Documentation.ps1
@@ -9,6 +9,7 @@ Copy-Item "..\Android-Utils.psm1" -Destination ".\modules\Android-Utils"
 Copy-Item "..\Android-Utils.psd1" -Destination ".\modules\Android-Utils"
 wget "https://raw.githubusercontent.com/gravejester/Invoke-CreateModuleHelpFile/master/Invoke-CreateModuleHelpFile.ps1" -outfile "Invoke-CreateModuleHelpFile.ps1"
 # Generate the documentation HTML file
+Get-ChildItem -Path . -Recurse -Depth 5
 . .\Invoke-CreateModuleHelpFile.ps1
 Import-Module -Name .\modules\Android-Utils
 Invoke-CreateModuleHelpFile -ModuleName "Android-Utils" -Path "$cdir\index.html"
