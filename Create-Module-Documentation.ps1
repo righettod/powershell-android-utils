08f251f5a960277246edb7f1a78d278bc7239350
Dominique RIGHETTO <righettod@users.noreply.github.com>
2023-11-14 18:03:01 +0100

    Update Create-Module-Documentation.ps1

--- Create-Module-Documentation.ps1
+++ Create-Module-Documentation.ps1
@@ -7,7 +7,7 @@ New-Item -Path "." -Name "modules" -ItemType "directory"
 New-Item -Path "modules" -Name "Android-Utils" -ItemType "directory"
 Copy-Item "..\Android-Utils.psm1" -Destination ".\modules\Android-Utils"
 Copy-Item "..\Android-Utils.psd1" -Destination ".\modules\Android-Utils"
-wget "https://raw.githubusercontent.com/gravejester/Invoke-CreateModuleHelpFile/master/Invoke-CreateModuleHelpFile.ps1" -outfile "Invoke-CreateModuleHelpFile.ps1"
+wget "https://raw.githubusercontent.com/righettod/Invoke-CreateModuleHelpFile/master/Invoke-CreateModuleHelpFile.ps1" -outfile "Invoke-CreateModuleHelpFile.ps1"
 # Generate the documentation HTML file
 Get-ChildItem -Path . -Recurse -Depth 5
 . .\Invoke-CreateModuleHelpFile.ps1
